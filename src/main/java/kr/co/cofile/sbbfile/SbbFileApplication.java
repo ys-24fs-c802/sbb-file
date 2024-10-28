63d9785a932fca3aa876bf4148f9d74835c7e993
shinis(home) <actionshin@gmail.com>
2024-10-29 00:21:35 +0900

    프로젝트 초기화

---
+++ src/main/java/kr/co/cofile/sbbfile/SbbFileApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbbfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbFileApplication.class, args);
+    }
+
+}
